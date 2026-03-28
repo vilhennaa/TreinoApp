567dda7ce16ddef39fd0891fa95d1a37448941e8
felipervnk <felipervnk@gmail.com>
2026-03-28 11:51:48 -0300

    feat: implementa tema dark mode com paleta laranja

--- app/src/main/java/com/gustavofelipe/treino/ui/theme/Color.kt
+++ app/src/main/java/com/gustavofelipe/treino/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.gustavofelipe.treino.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkSurfaceVariant = Color(0xFF2C2C2C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val NeonOrange = Color(0xFFFF5722)
+val NeonOrangeDark = Color(0xFFE64A19)
+val NeonOrangeLight = Color(0xFFFF8A65)
+val NeonOrangeContainer = Color(0xFFFFCC80)
+val OnNeonOrangeContainer = Color(0xFF3E2723)
